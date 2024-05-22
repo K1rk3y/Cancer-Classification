9ce0963814bfd7f7a0e737724acae86ff00bb779
UWAcarab <152263927+UWAcarab@users.noreply.github.com>
2024-05-22 10:47:12 +0800

    Update 4402GUI.py

--- 4402GUI.py
+++ 4402GUI.py
@@ -25,12 +25,19 @@ class ImageGUI:
 
          # Initiate global variables
          self.slice_directory = None
-         self.slice_value = 0
+         self.h5_files = None
+
          self.channel_idx = 1
          self.annotation_value = "OFF"
          self.alpha = 0.5
-         self.slider_min = 0
-         self.slider_max = 154
+
+         self.slice_value = 0
+         self.slice_slider_min = 0
+         self.slice_slider_max = 154
+
+         self.volume_value = 1
+         self.volume_slider_min = 1
+         self.volume_slider_max = 369
     
          # Create a frame for the GUI and center it
          self.frame = tk.Frame(self.master)
@@ -50,97 +57,109 @@ class ImageGUI:
          self.loadslicedirectory_button = tk.Button(self.border, text="Load Slice Directory", command=self.load_slice_directory, font=self.custom_font1)
          self.loadslicedirectory_button.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
+         # Label volume slider widget
+         self.volume_slider_label = tk.Label(self.border, text="Select Volume:", font=self.custom_font1)
+         self.volume_slider_label.grid(row=2, column=0, padx=5, pady=5)
+
+         self.volume_slider = tk.Scale(self.border, from_=self.volume_slider_min, to=self.volume_slider_max, orient=tk.HORIZONTAL, length=200,  command=self.update_volume_value, font=self.custom_font2)
+         self.volume_slider.grid(row=2, column=1, padx=5, pady=5)
+
          # Create annotation dropdown widget
          self.annotation_label = tk.Label(self.border, text="Annotation:", font=self.custom_font1)
-         self.annotation_label.grid(row=2, column=0, padx=5, pady=5)
+         self.annotation_label.grid(row=3, column=0, padx=5, pady=5)
         
          annotation_options = ["OFF", "ON"]                                       # <----------------------- Define here or in function or global variable?
          self.annotation_dropdown = ttk.Combobox(self.border, values=annotation_options, font=self.custom_font2)
          self.annotation_dropdown.set(annotation_options[0]) # Set initial value to OFF
          self.annotation_dropdown.bind("<<ComboboxSelected>>", self.update_annotation_value) # Bind update_annotation_value function to change in annotation_dropdown
-         self.annotation_dropdown.grid(row=2, column=1, padx=5, pady=5)
+         self.annotation_dropdown.grid(row=3, column=1, padx=5, pady=5)
 
          # Create a channel selection dropdown widget
          self.channel_label = tk.Label(self.border, text="Select Channel:", font=self.custom_font1)
-         self.channel_label.grid(row=3, column=0, padx=5, pady=5)
+         self.channel_label.grid(row=4, column=0, padx=5, pady=5)
         
          channel_options = ["T2-FLAIR", "T1", "T1-Gd", "T2"]                                       # <----------------------- Define here or in function or global variable?
          self.channel_dropdown = ttk.Combobox(self.border, values=channel_options, font=self.custom_font2)
          self.channel_dropdown.set(channel_options[1]) # Set initial value to T1
          self.channel_dropdown.bind("<<ComboboxSelected>>", self.update_channel_selection) # Bind update_channel_selection function to change in channel_dropdown
-         self.channel_dropdown.grid(row=3, column=1, padx=5, pady=5)
+         self.channel_dropdown.grid(row=4, column=1, padx=5, pady=5)
 
          # Label slice slider widget
          self.slicer_slider_label = tk.Label(self.border, text="Select Slice:", font=self.custom_font1)
-         self.slicer_slider_label.grid(row=4, column=0, padx=5, pady=5)
+         self.slicer_slider_label.grid(row=5, column=0, padx=5, pady=5)
 
-         self.slice_slider = tk.Scale(self.border, from_=self.slider_min, to=self.slider_max, orient=tk.HORIZONTAL, length=200,  command=self.update_slice_value, font=self.custom_font2)
-         self.slice_slider.grid(row=4, column=1, padx=5, pady=5)
-
-         # Create a blank canvas on which to show image
-         self.canvas = tk.Canvas(self.border, width=241, height=241, bg="black")
-         self.canvas.grid(row=1, rowspan=6, column=2, columnspan=2, padx=5, pady=5)
+         self.slice_slider = tk.Scale(self.border, from_=self.slice_slider_min, to=self.slice_slider_max, orient=tk.HORIZONTAL, length=200,  command=self.update_slice_value, font=self.custom_font2)
+         self.slice_slider.grid(row=5, column=1, padx=5, pady=5)
 
          # Extract conventional features button
          self.extractconventionalfeatures_button = tk.Button(self.border, text="Extract Conventional Features", command=self.extract_conventional_features, font=self.custom_font1)
-         self.extractconventionalfeatures_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+         self.extractconventionalfeatures_button.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
 
          # Extract radiomic features button
          self.extractradiomicfeatures_button = tk.Button(self.border, text="Extract Radiomic Features", command=self.extract_radiomic_features, font=self.custom_font1)
-         self.extractradiomicfeatures_button.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
+         self.extractradiomicfeatures_button.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+         
+         # Create a blank canvas on which to show image
+         self.canvas = tk.Canvas(self.border, width=241, height=241, bg="black")
+         self.canvas.grid(row=1, rowspan=7, column=2, columnspan=2, padx=5, pady=5)
          
          
 
     ## ___________________________________________________________________________________ Show image function
     def show_image(self):
         # Required image
-        file_directory = f"{self.slice_directory}/slice_{self.slice_value}.h5"
+        file_directory = f"{self.slice_directory}/volume_{self.volume_value}_slice_{self.slice_value}.h5"
 
-        figure, axes = plt.subplots()
-        figure.patch.set_facecolor('#F0F0F0')
+        # Print error and don't refresh if image not found
+        if not os.path.exists(file_directory):
+            print("ERROR: File not found! Image not updated")
 
-        # Close previous figure
-        plt.close(figure)
+        else:
+            figure, axes = plt.subplots()
+            figure.patch.set_facecolor('#F0F0F0')
 
-        # Create new figure
-        with h5py.File(file_directory, 'r') as file:
-            image = file["image"]
+            # Close previous figure
+            plt.close(figure)
 
-            # Make numpy array
-            imageArray = np.array(image)
-            # Normalise
-            normimageArray = cv2.normalize(imageArray[:,:,self.channel_idx], None, norm_type=cv2.NORM_MINMAX)
-            # Show image from selected channel
-            axes.imshow(normimageArray, cmap='gray')
-            
-            # Check if annotation is ON
-            if self.annotation_value == "ON":
-                # Annotation is ON then Overlay mask
-                mask = file["mask"]
-                # Make a composite of all 3 channels of the mask segmentation
-                mask_comp = np.sum(mask, axis=2)
-
-                # Ensure compatible with cv2.normalise
-                if mask_comp.dtype != np.float32:
-                    mask_comp = mask_comp.astype(np.float32)
+            # Create new figure
+            with h5py.File(file_directory, 'r') as file:
+                image = file["image"]
 
                 # Make numpy array
-                maskcompArray = np.array(mask_comp)
+                imageArray = np.array(image)
                 # Normalise
-                normmaskcompArray = cv2.normalize(maskcompArray, None, norm_type=cv2.NORM_MINMAX)
+                normimageArray = cv2.normalize(imageArray[:,:,self.channel_idx], None, norm_type=cv2.NORM_MINMAX)
+                # Show image from selected channel
+                axes.imshow(normimageArray, cmap='gray')
+            
+                # Check if annotation is ON
+                if self.annotation_value == "ON":
+                    # Annotation is ON then Overlay mask
+                    mask = file["mask"]
+                    # Make a composite of all 3 channels of the mask segmentation
+                    mask_comp = np.sum(mask, axis=2)
 
-                # Overlay mask
-                axes.imshow(normmaskcompArray*255, cmap='gray', alpha=self.alpha)
+                    # Ensure compatible with cv2.normalise
+                    if mask_comp.dtype != np.float32:
+                        mask_comp = mask_comp.astype(np.float32)
 
-            axes.axis("off")
-            figure.tight_layout()
+                    # Make numpy array
+                    maskcompArray = np.array(mask_comp)
+                    # Normalise
+                    normmaskcompArray = cv2.normalize(maskcompArray, None, norm_type=cv2.NORM_MINMAX)
 
-            # Create tkinter canvas
-            canvas = FigureCanvasTkAgg(figure, master=self.canvas)
-            canvas.draw()
+                    # Overlay mask
+                    axes.imshow(normmaskcompArray*255, cmap='gray', alpha=self.alpha)
 
-            # Pack new canvas into GUI window
-            canvas.get_tk_widget().grid(row=1, rowspan=5, column=2, columnspan=2)
+                axes.axis("off")
+                figure.tight_layout()
+
+                # Create tkinter canvas
+                canvas = FigureCanvasTkAgg(figure, master=self.canvas)
+                canvas.draw()
+
+                # Pack new canvas into GUI window
+                canvas.get_tk_widget().grid(row=1, rowspan=5, column=2, columnspan=2)
 
     ## ___________________________________________________________________________________ Load slice directory function
     def load_slice_directory(self):
@@ -151,23 +170,10 @@ class ImageGUI:
         print(f"    output: slice_directory = '{self.slice_directory}'")
 
         # Get min and max slice values
-        h5_files = os.listdir(self.slice_directory)
+        self.h5_files = os.listdir(self.slice_directory)
         # Filter only .h5 files
-        h5_files = [file for file in h5_files if file.endswith(".h5")]
-        slice_numbers = [int(file.split("_")[1].split(".")[0]) for file in h5_files]
-
-        self.slider_min = min(slice_numbers)
-        self.slider_max = max(slice_numbers)
-        self.slice_slider = tk.Scale(self.border, from_=self.slider_min, to=self.slider_max, orient=tk.HORIZONTAL, length=200,  command=self.update_slice_value, font=self.custom_font2)
-        self.slice_slider.grid(row=4, column=1, padx=5, pady=5)
-        
-        if self.slice_value >= self.slider_max:
-            self.slice_value = self.slider_max
-            self.slice_slider.set(self.slice_value)
-        if self.slice_value <= self.slider_min:
-            self.slice_value = self.slider_min
-            self.slice_slider.set(self.slice_value)
-            
+        self.h5_files = [file for file in self.h5_files if file.endswith(".h5")]
+                 
         # Update image
         self.show_image()
 
@@ -198,6 +204,15 @@ class ImageGUI:
     def update_slice_value(self, event):
         # Retrieve slice value from slider
         self.slice_value = self.slice_slider.get()
+
+        if self.slice_directory is not None:
+            # Update image
+            self.show_image()
+    
+    ## ___________________________________________________________________________________ Function to update slice value
+    def update_volume_value(self, event):
+        # Retrieve slice value from slider
+        self.volume_value = self.volume_slider.get()
 
         if self.slice_directory is not None:
             # Update image
